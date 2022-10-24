6d7f7f7a61587679befc3d89eafa443f36f19f23
heshamouda <hesham.ouda@outlook.com>
2022-10-24 13:47:06 +0200

    Add toSTring method in class Book for all properities

--- SpringWebApp/spring5webapp/src/main/java/com/heshamouda/domain/Book.java
+++ SpringWebApp/spring5webapp/src/main/java/com/heshamouda/domain/Book.java
@@ -70,4 +70,9 @@ public class Book {
         this.id = id;
     }
 
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "]";
+    }
+
 }
