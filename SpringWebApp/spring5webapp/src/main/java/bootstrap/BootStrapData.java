5b0fb55d68c9834a0fba594145d6debc038b0204
heshamouda <hesham.ouda@outlook.com>
2022-10-24 14:33:22 +0200

    create new class for bootstrap data

---
+++ SpringWebApp/spring5webapp/src/main/java/bootstrap/BootStrapData.java
@@ -0,0 +1,16 @@
+package bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * created by hesham Ouda on 24/10/2022
+ */
+public class BootStrapData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
